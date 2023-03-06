8c217d23cedb6dc45a3b1c24ba70f447c7289a9c
vinicius <vmm_7@hotmail.com>
2023-03-06 19:21:03 -0300

    Setando todos os inputs e métodos da aplicação

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+            string operation = Console.ReadLine();
+
 
         }
     }
