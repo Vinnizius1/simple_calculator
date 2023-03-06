bbec102865c4ea7a50174fb8595074637695cafb
vinicius <vmm_7@hotmail.com>
2023-03-06 19:35:04 -0300

    Criação da classe 'InputConverter'

---
+++ CalculatorEngine.cs
@@ -0,0 +1,6 @@
+namespace SimpleCalculator
+{
+    public class CalculatorEngine
+    {
+    }
+}
---
+++ InputConverter.cs
@@ -0,0 +1,12 @@
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value.");
+            return convertedNumber;
+        }
+    }
+}
